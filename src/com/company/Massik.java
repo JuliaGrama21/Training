04a984a139736354b995ac169b44f16f155e7c64
JuliaGrama <Grama98@email.ua>
2018-02-22 21:40:42 +0200

    Fix problem

--- src/com/company/Massik.java
+++ src/com/company/Massik.java
@@ -5,16 +5,16 @@ public class Massik {
     public static void main(String[] args) {
 
         int s = 0;
-        int d = 0;
+        int d = 1;
         int k = 0;
 
         int mas[] = {1, 2, 5, 0, -4, -8, 12, 45, 3};
-        for (int i = 0; i <mas.length ; i++) {
+        for (int i = 0; i < mas.length; i++) {
             if (mas[i] > 0){
-                s++;
+                s = s + mas[i];
             }
             if (mas[i] < 0){
-                d++;
+                d = d * mas[i];
             }
             if (mas[i] == 0){
                 k++;
